afeaf4f12865fbebb16a422ba65533d505ceb7f0
Sanjay5734 <sanjay57342246@gmail.com>
2025-03-25 15:24:40 +0530

    third

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ app.use("/images", express.static("upload/images"));
 app.post("/upload", upload.single("product"), (req, res) => {
   res.json({
     success: 1,
-    image_url: `${url}/images/${req.file.filename}`,
+    image_url: url+/images/+req.file.filename,
   });
 });
 
